77e337894a87dce1aa2ecead26c3aa10e4c076c5
SnottyJACK <snottyjack@yandex.ru>
2020-09-22 20:24:31 +0500

    Final modified

--- articles/models.py
+++ articles/models.py
@@ -20,7 +20,10 @@ class Article(models.Model):
 
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article,
+    on_delete=models.CASCADE,
+    related_name='comments'
+    )
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
